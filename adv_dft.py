77c6a35746d2c1e11909e3ab19b90dff6d75c5d0
tactnician <tactnician@gmail.com>
2020-01-24 17:15:20 -0500

    adv_dft_recur completes graph but is missing managable backtrack solution

--- adv_dft.py
+++ adv_dft.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-from util import Stack
+from util import Stack, Queue
 
 import random
 from ast import literal_eval
@@ -11,9 +11,9 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-map_file = "maps/test_loop.txt"
+# map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
@@ -31,7 +31,10 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
-
+traversal_graph = {}
+for i in room_graph:
+    traversal_graph[i] = room_graph[i][1]
+print(f'traversal graph / {traversal_graph}')
 
 # ---------------------------------------------------
             # DFT Procedure 
@@ -61,9 +64,45 @@ traversal_path = []
 # ---------------------------------------------------
             # Traversal Graph
 # ---------------------------------------------------
-# def bft():
+# # def bft():
+# stack = Stack() # Create a queue/stack as appropriate
+# stack.push(player.current_room.id) # Put the starting point in that
+# visited_rooms = set() # Make a set to keep track of where we’ve been
+# player.current_room = world.starting_room
+
+# while stack.size() > 0: # While there is stuff in the queue/stack
+#     curr_room = stack.pop() # Pop the first item
+    
+#     if curr_room not in visited_rooms: # If not visited
+#         # DO THE THING! Implement README Procedure
+        
+#         exits = player.current_room.get_exits()
+#         # pick a random unexplored direction from the player's current room
+#         new_direction = exits[random.randint(0, len(exits) - 1)]
+#         print(f"new direction / {new_direction}") # island Problem? 
+#         traversal_path.append(new_direction)  # travel and log that direction
+#         visited_rooms.add(curr_room)
+#         for move in traversal_path:
+#             player.travel(move)
+#             stack.push(player.current_room.id)
+
+#         # TODO take shortest path to room with unexplored paths ( utilizes bft?)
+#         # maybe convert above for loop into bft 
+
+
+# ---------------------------------------------------
+            # Traversal DFT w/ Search BFT
+# ---------------------------------------------------
+# dft with bfs for next empty room 
+#
+
 stack = Stack() # Create a queue/stack as appropriate
-stack.push(player.current_room.id) # Put the starting point in that
+que = Queue()
+
+# Put the starting point in that
+# Starting point in coordinate form ( 0, 0)
+room_coord = room_graph[player.current_room.id][0]
+stack.push(player.current_room.id) 
 visited_rooms = set() # Make a set to keep track of where we’ve been
 # player.current_room = world.starting_room
 
@@ -82,12 +121,12 @@ while stack.size() > 0: # While there is stuff in the queue/stack
         for move in traversal_path:
             player.travel(move)
             stack.push(player.current_room.id)
-
-        # TODO take shortest path to room with unexplored paths ( utilizes bft?)
-        # maybe convert above for loop into bft 
-
-
             
+            que.enqueue(player.current_room.id) #Begin BFS for unvisited rooms
+            while que.size > 0: 
+                room_curr = que.dequeue()
+                if room_curr not in visited_rooms:
+                    #travel to that room 
 
 # for move in traversal_path:
 #     player.travel(move)
--- adv_dft_recur.py
+++ adv_dft_recur.py
@@ -29,24 +29,132 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+traversal_graph = {}
+
+# ---------------------------------------------------
+            # DFT from Notes
+# ---------------------------------------------------
+# def dft_recursive(self, starting_vertex, visited=None):
+#         """
+#         Print each vertex in depth-first order
+#         beginning from starting_vertex.
+#         This should be done using recursion.
+#         """
+#         if visited is None:
+#             visited = set()
+#         visited.add(starting_vertex)
+#         print(starting_vertex)
+#         for child_vert in self.vertices[starting_vertex]:
+#             if child_vert not in visited:
+#                 self.dft_recursive(child_vert, visited)
+    
+
+# ---------------------------------------------------
+            # Traversal Graph
+# ---------------------------------------------------
+def recur(starting_room, visited_rooms=None):
+    if visited_rooms is None: 
+        visited_rooms = set()
+    
+    visited_rooms.add(starting_room.id)
+    print(f'visited rooms / {visited_rooms}')
+    traversal_graph[starting_room.id] = room_graph[starting_room.id][1]
+    print(f'traversal graph / {traversal_graph}')
+    exits = player.current_room.get_exits()
+    print(f'exits / {exits}')
+
+    opp_dir = {
+        'n' : 's',
+        's' : 'n',
+        'e' : 'w',
+        'w' : 'e'
+    }
+
+    for direction in exits:
+        has_direction = player.current_room.get_room_in_direction(direction)
+        if has_direction.id not in visited_rooms:
+            # print(f'player.travel direction / {player.travel(direction)}')
+            # if has_direction.id is None:
+            #     continue
+            # if len(exits) is 1:
+            #     player.travel(exits[0])
+            #     visited_rooms.add(player.current_room.id)
+            #     traversal_path.append(exits[0])
+            #     print(f'traversal path / {traversal_path}')
+            #     recur(player.current_room, visited_rooms)
+            
+            player.travel(direction)
+            visited_rooms.add(player.current_room.id)
+            traversal_path.append(direction)
+            print(f'traversal path / {traversal_path} \n')
+            recur(player.current_room, visited_rooms)
+        elif has_direction.id in visited_rooms:
+            if len(exits) >= 1:
+                player.travel(exits[random.randint(0, len(exits)-1)])
+                visited_rooms.add(player.current_room.id)
+                traversal_path.append(exits[0])
+                print(f'traversal path / {traversal_path} \n')
+                recur(player.current_room, visited_rooms)
+
+        
+        
+        # elif has_direction.id is None or has_direction.id in visited_rooms:
+        #     if direction is 'n':
+        #         player.travel('s')
+        #         traversal_path.append(direction)
+        #         recur(player.current_room, visited_rooms)
+        #     if direction is 's':
+        #         player.travel('n')
+        #         traversal_path.append(direction)
+        #         recur(player.current_room, visited_rooms)
+        #     if direction is 'e':
+        #         player.travel('w')
+        #         traversal_path.append(direction)
+        #         recur(player.current_room, visited_rooms)
+        #     if direction is 'w':
+        #         player.travel('e')
+        #         traversal_path.append(direction)
+        #         recur(player.current_room, visited_rooms)
 
 
+    # if traversal_graph[starting_room.id]['n'] is not '?' or None:
+    #     player.travel('n')
+    #     print(f'travel to room north / {player.current_room}')
+    #     recur(player.current_room, visited_rooms)
 
-# TRAVERSAL TEST
-visited_rooms = set()
-player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
+    # if traversal_graph[starting_room.id]['s'] is not '?' or None:
+    #     player.travel('s')
+    #     print(f'travel to room south / {player.current_room}')
+    #     recur(player.current_room, visited_rooms)
 
-for move in traversal_path:
-    player.travel(move)
-    visited_rooms.add(player.current_room)
+    # if traversal_graph[starting_room.id]['e'] is not '?' or None:
+    #     player.travel('e')
+    #     print(f'travel to room east / {player.current_room}')
+    #     recur(player.current_room, visited_rooms)
+        
+    # if traversal_graph[starting_room.id]['w'] is not '?' or None:
+    #     player.travel('w')
+    #     print(f'travel to room west / {player.current_room}')
+    #     recur(player.current_room, visited_rooms)
 
-if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
-else:
-    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+    
 
+    # for room in traversal_graph:
+    #     if room not in visited_rooms:
+    #         recur(room, visited_rooms)
+
+
+# for move in traversal_path:
+#     player.travel(move)
+#     visited_rooms.add(player.current_room)
+
+        elif len(visited_rooms) == len(room_graph):
+            print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+        else:
+            print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+            print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+
+recur(player.current_room)
 
 
 #######
