537ceeedfaf8b04d4c9392c0007163c036f79b16
tactnician <tactnician@gmail.com>
2020-02-22 19:50:02 -0500

    built method to return list of edge tuples

--- cc.py
+++ cc.py
@@ -17,59 +17,38 @@ room_graph=literal_eval(open(map_file, "r").read())
 class Graph: 
       
     # init function to declare class variables 
-    def __init__(self,V): 
-        self.V = V # Graph Length
+    def __init__(self,init_graph): 
+        # self.V = V # Graph Length
+        
+        # self.adj = [[] for i in range(V)] #return array for each item 
+        self.init_graph = init_graph
         self.traversal_graph = {}
-        self.adj = [[] for i in range(V)] #return array for each item 
+        self.traversal_path = []
+        self.path = []
+        self.visited = []
+        self.edgeList = []
 
-  
     def buildGraph(self, room_graph):
         for k in room_graph:
             self.traversal_graph[k] = room_graph[k][0]
         print(f'Traversal_graph: {self.traversal_graph}')
 
-        for k in self.traversal_graph: 
-            x = self.traversal_graph[k][0]
-            y = self.traversal_graph[k][1]
-            self.addEdge(x,y)
+        # for k in self.traversal_graph: 
+        #     x = self.traversal_graph[k][0]
+        #     y = self.traversal_graph[k][1]
+        #     self.addEdge(x,y)
+
     
+    def buildEdgeList(self):
+        for k in self.traversal_graph:
+            self.edgeList.append(self.traversal_graph[k])
+        print(f'Edge List: {self.edgeList}')
+    
+    # def dft(starting_vertex):
+    #     while len(self.visited) < len(self.traversal_graph):
+    #         pass
+            
 
-    def DFSUtil(self, temp, v, visited): 
-  
-        # Mark the current vertex as visited 
-        visited[v] = True
-  
-        # Store the vertex to list 
-        temp.append(v) 
-  
-        # Repeat for all vertices adjacent 
-        # to this vertex v 
-        for i in self.adj[v]: 
-            if visited[i] == False:
-
-                # Update the list 
-                temp = self.DFSUtil(temp, i, visited) 
-        return temp 
-  
-    # method to add an undirected edge 
-    def addEdge(self, v, w): 
-        self.adj[v].append(w) 
-        self.adj[w].append(v) 
-  
-    # Method to retrieve connected components 
-    # in an undirected graph 
-    def connectedComponents(self): 
-        visited = [] 
-        cc = [] 
-        for i in range(self.V): 
-            visited.append(False) 
-        for v in range(self.V): 
-            if visited[v] == False: 
-                temp = [] 
-                cc.append(self.DFSUtil(temp, v, visited)) 
-        return cc 
-        
-  
 # Driver Code 
 if __name__=="__main__": 
       
@@ -78,7 +57,7 @@ if __name__=="__main__":
     g = Graph(len(room_graph))
     
     g.buildGraph(room_graph)
-
+    g.buildEdgeList()
     # g.addEdge(3,5)
     # g.addEdge(3,6) 
     # g.addEdge(3,7) 
@@ -99,6 +78,47 @@ if __name__=="__main__":
     # g.addEdge(1,7) 
     
     # cc = g.connectedComponents() 
-    print("Following are connected components") 
-    print(cc) 
+    # print("Following are connected components") 
+    # print(cc) 
+  
+
+    #   def DFSUtil(self, temp, v, visited): 
+  
+    #     # Mark the current vertex as visited 
+    #     visited[v] = True
+  
+    #     # Store the vertex to list 
+    #     temp.append(v) 
+  
+    #     # Repeat for all vertices adjacent 
+    #     # to this vertex v 
+    #     for i in self.adj[v]: 
+    #         if visited[i] == False:
+
+    #             # Update the list 
+    #             temp = self.DFSUtil(temp, i, visited) 
+    #     print(f'Temp: {temp}')
+    #     return temp 
+  
+    # # method to add an undirected edge 
+    # def addEdge(self, v, w): 
+    #     self.adj[v].append(w) 
+    #     self.adj[w].append(v) 
+  
+    # # Method to retrieve connected components 
+    # # in an undirected graph 
+    # def connectedComponents(self): 
+    #     visited = [] 
+    #     cc = [] 
+    #     for i in range(self.V): 
+    #         visited.append(False) 
+    #     for v in range(self.V): 
+    #         if visited[v] == False: 
+    #             temp = [] 
+    #             cc.append(self.DFSUtil(temp, v, visited)) 
+    #     return cc 
+        
+    # def path_components(self):
+    #     pass
+
   
